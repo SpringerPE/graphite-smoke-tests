6dea79253acd9c3b137157506735750ece06c8a4
Claudio Benfatto <claudio.benfatto@gmail.com>
2017-03-22 17:21:21 +0000

    rename helpers file

--- smoke/graphite_helpers.go
+++ smoke/graphite_helpers.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"strconv"
 	"strings"
+	"fmt"
 )
 
 type GraphiteClient struct {
@@ -38,53 +39,65 @@ func (d *Datapoint) Value() string {
 	return s
 }
 
-func NewGraphiteClient(host string, port int, apiHost string, protocol string, prefix string) *GraphiteClient {
+func NewGraphiteClient(host string, port int, apiHost string, protocol string, prefix string) (*GraphiteClient, error) {
 	var client *GraphiteClient
 
-	g, _ :=  graphite.GraphiteFactory(protocol, host, port, prefix)
-	client = &GraphiteClient{Api: apiHost, Graphite: g}
-	return client
-}
-
-func (client *GraphiteClient) SendMetricToGraphite(name string, value string) int {
-
-	log.Printf("Loaded Graphite connection: %#v", client)
-	client.SimpleSend(name, value)
-	return 0
-}
-
-func (client *GraphiteClient) GetMetricFromGraphite(name string) (ApiResponse, error) {
-	httpClient := &http.Client{}
-
-	api_endpoint := strings.Join([]string{"http://", client.Api, "/render"}, "")
-	req, err := http.NewRequest("GET", api_endpoint, nil)
+	g, err :=  graphite.GraphiteFactory(protocol, host, port, prefix)
 	if err != nil {
-		log.Printf("Error while creating a new GET request for endpoint %s", api_endpoint)
 		return nil, err
 	}
+	client = &GraphiteClient{Api: apiHost, Graphite: g}
+	return client, nil
+}
+
+func (client *GraphiteClient) SendMetricToGraphite(name string, value string) error{
+
+	log.Printf("Loaded Graphite connection: %#v", client)
+	log.Printf("Original graphite client: %#v", client.Graphite)
+
+	err := client.SimpleSend(name, value)
+	if err != nil {
+		return err
+	}
+	err = client.Graphite.Disconnect()
+	return err
+}
+
+func (client *GraphiteClient) GetMetricFromGraphite(name string, from int) (ApiResponse, error) {
+	httpClient := &http.Client{}
+
+	apiEndpoint := strings.Join([]string{"http://", client.Api, "/render"}, "")
+	req, err := http.NewRequest("GET", apiEndpoint, nil)
+	if err != nil {
+		log.Printf("Error while creating a new GET request for endpoint %s", apiEndpoint)
+		return nil, err
+	}
+
 	q := req.URL.Query()
 	q.Add("target", strings.Join([]string{client.Prefix, name}, "."))
 	q.Add("format", "json")
-	q.Add("from", "-5min")
+	q.Add("from", fmt.Sprintf("-%dmin", from))
 	req.URL.RawQuery = q.Encode()
 	resp, err := httpClient.Do(req)
-	if err != nil {
-		log.Printf("Error while attempting a request to %s endpoint", api_endpoint)
-		return nil, err
-	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Printf("Error while reading the response body for request to %s endpoint", api_endpoint)
-		return nil, err
-	}
-	dec_response, err := DecodeApiResponse(body)
-	if err != nil {
-		log.Printf("Error while decoding the json api response for request to %s", api_endpoint)
+		log.Printf("Error while attempting a request to %s endpoint", apiEndpoint)
 		return nil, err
 	}
 
-	return dec_response, nil
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Error while reading the response body for request to %s endpoint", apiEndpoint)
+		return nil, err
+	}
+
+	decResponse, err := DecodeApiResponse(body)
+	if err != nil {
+		log.Printf("Error while decoding the json api response for request to %s", apiEndpoint)
+		return nil, err
+	}
+
+	return decResponse, nil
 }
 
 func DecodeApiResponse(respBody []byte) (ApiResponse, error) {
