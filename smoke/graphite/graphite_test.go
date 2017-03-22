dd0b7f886a203f4e1e8c1514144c4eda85202a55
Claudio Benfatto <claudio.benfatto@gmail.com>
2017-03-22 17:22:56 +0000

    improve smoke tests

--- smoke/graphite/graphite_test.go
+++ smoke/graphite/graphite_test.go
@@ -7,6 +7,7 @@ import (
 	"time"
 	"fmt"
 	"strconv"
+	"log"
 )
 
 
@@ -19,66 +20,103 @@ var _ = Describe("Graphite:", func() {
 		gclient *smoke.GraphiteClient
 	)
 
+	if testConfig.TcpEnabled {
+		Context("send single tcp metric value", func() {
 
-	BeforeEach(func() {
+	    		var err error
+	    		protocol := "tcp"
 
+			gclient, err = smoke.NewGraphiteClient(
+				testConfig.Host, 
+				testConfig.Port,
+				testConfig.Api,
+				protocol,
+				testPrefix,
+			)
 
+			if err != nil {
+				Fail(err.Error())
+			}
+
+			It("can be sent over tcp and retrieved", func() {
+				runSendAndRetrieveTest(gclient, protocol)
+			})
 		})
+	}
 
-	AfterEach(func() {
+	if testConfig.UdpEnabled {
+		Context("send single udp metric value", func() {
 
 
+	    		var err error
+	    		protocol := "udp"
+
+			gclient, err = smoke.NewGraphiteClient(
+				testConfig.Host, 
+				testConfig.Port,
+				testConfig.Api,
+				protocol,
+				testPrefix,
+			)
+
+			if err != nil {
+				Fail(err.Error())
+			}
+
+			It("can be sent over udp and retrieved", func() {
+		    		if ! testConfig.UdpEnabled {
+		        			Skip("udp not enabled, skip test")
+		    		}
+				runSendAndRetrieveTest(gclient, protocol)
+			})
 		})
-
-	Context("send single tcp metric value", func() {
-
-		gclient = smoke.NewGraphiteClient(
-			testConfig.Host, 
-			testConfig.Port,
-			testConfig.Api,
-			testConfig.Proto,
-			testPrefix,
-		)
-
-		It("can be sent over tcp and retrieved", func() {
-			runSendAndRetrieveTest(gclient, "tcp_metric")
-		})
-	})
+	}
 })
 
-func sendMetric() int{
-	return 0
-}
+func runSendAndRetrieveTest(gclient *smoke.GraphiteClient, metricBase string) {
 
-func runSendAndRetrieveTest(gclient *smoke.GraphiteClient, metric_name string) {
-
-	var api_response smoke.ApiResponse
-	var err error
+	var (
+		apiResponse smoke.ApiResponse
+		err error
+		elapsed int
+	)
 
 	maxAttempts := 60
+	retryDelay := 10000
+	from := retryDelay / 1000
 	found := false
-
 	value := strconv.FormatInt(time.Now().UnixNano() / int64(time.Millisecond), 10)
-	gclient.SendMetricToGraphite(metric_name, value)
+	metricName := fmt.Sprintf("%s.%s", metricBase , value)
+	expectedOutput := fmt.Sprintf("datapoint: %s, for metric %s", value, metricName)
 
-	expectedOutput := fmt.Sprintf("datapoint: %s, for metric %s", value, metric_name)
-
+	//Start the test
+	err = gclient.SendMetricToGraphite(metricName, value)
+	if err != nil {
+		Fail(fmt.Sprintf("Error while executing the test: %s\n", err.Error()))
+	}
+	elapsed = 0
 	Loop:
 		for j := 0; j < maxAttempts; j++ {
-			api_response, err = gclient.GetMetricFromGraphite(metric_name)
+			apiResponse, err = gclient.GetMetricFromGraphite(metricName, from)
+			log.Printf("Response from the api server: %v\n", apiResponse)
+			log.Printf("Expected output: %s\n", expectedOutput)
 			if err != nil {
 				break Loop
 			}
-			for _, metric := range(api_response) {
+			for _, metric := range(apiResponse) {
 				for _, datapoint := range(metric.Datapoints) {
+					fmt.Printf("%v\n", datapoint)
 					if datapoint.Value() == value {
 						found = true
 						break Loop
 					}
 				}
+				log.Printf("%s not found in: %v\n", expectedOutput, apiResponse)
 			}
-			time.Sleep(5000 * time.Millisecond)
+			time.Sleep(time.Duration(retryDelay) * time.Millisecond)
+			elapsed += retryDelay
+			log.Printf("Attempt #%d, sec elapsed: %d\n", j+1, elapsed/1000)
 		}
 
-	Expect(found).To(BeTrue(), fmt.Sprintf("Wanted to see '%s' (datapoint found) in %d attempts, but didn't", expectedOutput, maxAttempts))
+	Expect(found).To(BeTrue(), fmt.Sprintf("Wanted to see '%s' (datapoint found) in %d attempts, but didn't\n", expectedOutput, maxAttempts))
 }
